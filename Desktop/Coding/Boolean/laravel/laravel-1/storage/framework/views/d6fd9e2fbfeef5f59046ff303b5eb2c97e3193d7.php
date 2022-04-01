8a588b40dbdfbf7575034f654dba23b8659c7d1c
EmanuelaMocci <emanuelamocci2@gmail.com>
2022-04-01 15:02:49 +0200

    vue weather

---
+++ Desktop/Coding/Boolean/laravel/laravel-1/storage/framework/views/d6fd9e2fbfeef5f59046ff303b5eb2c97e3193d7.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Home Page</h1>
+
+    <ul>
+        <li><a href="/">Home Page</a></li>
+        <li><a href="/chi-siamo">Chi siamo</a></li>
+        <li><a href="/contatti">Contatti</a></li>
+
+    </ul>
+
+    <h2>Mi chiamo <?php echo e($nome); ?> <?php echo e($cognome); ?></h2>
+
+    <ul>
+        <?php $__currentLoopData = $amici; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $amico): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <li><?php echo e($amico); ?></li>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </ul>
+</body>
+</html><?php /**PATH /Users/emanuela/Desktop/Boolean/laravel/resources/views/index.blade.php ENDPATH**/ ?>
